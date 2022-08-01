3e2995d3e94057cee0ecb3b63ba2dd1139d659fd
Setsumiku <99953781+Setsumiku@users.noreply.github.com>
2022-08-01 14:10:34 +0200

    Implement GenericRepository and its interface

---
+++ Api/Api.Data/DAL/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿namespace Api.Data.DAL
+{
+    internal class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private ApiContext Context;
+        private readonly DbSet<T> entities;
+        public GenericRepository(ApiContext repositoryContext)
+        {
+            this.Context = repositoryContext;
+            entities = Context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(string[] includes)
+            => await includes.Aggregate(entities.AsQueryable(), (query, path) => query.Include(path)).ToListAsync();
+
+        public async Task<IEnumerable<T>> GetMultipleByConditionAsync(Expression<Func<T, bool>> ex, string[] includes)
+            => await includes.Aggregate(entities.Where(ex).AsQueryable(), (query, path) => query.Include(path)).ToListAsync();
+
+        public async Task<T> GetSingleByConditionAsync(Expression<Func<T, bool>> ex, string[] includes)
+            => await includes.Aggregate(entities.Where(ex).AsQueryable(), (query, path) => query.Include(path)).SingleOrDefaultAsync();
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            await Context.AddAsync(entity);
+            await Context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            Context.Set<T>().Update(entity);
+            await Context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<bool> DeleteAsync(T entity)
+        {
+            Context.Set<T>().Remove(entity);
+            await Context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
