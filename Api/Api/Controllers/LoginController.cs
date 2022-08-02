62fced964dde7c79ed94f6b6536303604606700e
Setsumiku <99953781+Setsumiku@users.noreply.github.com>
2022-08-02 13:51:47 +0200

    Implement loginController

--- Api/Api/Controllers/LoginController.cs
+++ Api/Api/Controllers/LoginController.cs
@@ -1,4 +1,7 @@
-﻿
+﻿using Api.Data.Model.Authentication;
+using Api.Data.Repository;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace Api.Controllers
 {
@@ -6,5 +9,58 @@ namespace Api.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
+        public IConfiguration _configuration;
+        private readonly IBaseRepository<User> _userRepository;
+        public LoginController(IConfiguration config, IBaseRepository<User> userRepository)
+        {
+            _configuration = config;
+            _userRepository = userRepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(string login, string password)
+        {
+            if (login != null && password != null)
+            {
+                var user = await GetUser(login, password);
+                if (user != null)
+                {
+                    //create claims details based on the user information
+                    var claims = new[] {
+                        new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                        new Claim("UserId", user.UserId.ToString()),
+                        new Claim("DisplayName", user.DisplayName),
+                        new Claim("UserName", user.UserName),
+                        new Claim("Email", user.Email)
+                    };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                    var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                    var token = new JwtSecurityToken(
+                        _configuration["Jwt:Issuer"],
+                        _configuration["Jwt:Audience"],
+                        claims,
+                        expires: DateTime.UtcNow.AddMinutes(10),
+                        signingCredentials: signIn);
+
+                    return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+                }
+                else
+                {
+                    return BadRequest("Invalid credentials");
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private async Task<User> GetUser(string login, string password)
+        {
+            return await _userRepository.GetSingleByConditionAsync(u => u.Credentials.Login == login && u.Credentials.Password == password);
+        }
     }
+}
 }
--- Api/Api/Program.cs
+++ Api/Api/Program.cs
@@ -60,21 +60,19 @@ builder.Services.AddSwaggerGen(options =>
         Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
     });
     options.AddSecurityRequirement(new OpenApiSecurityRequirement
- {
-     {
+    {
+        {
            new OpenApiSecurityScheme
-             {
-                 Reference = new OpenApiReference
-                 {
-                     Type = ReferenceType.SecurityScheme,
-                     Id = "Bearer",
-                 },
-             },
-             new string[] {}
-     }
- });
-
-    // using System.Reflection;
+           {
+               Reference = new OpenApiReference
+               {
+                   Type = ReferenceType.SecurityScheme,
+                   Id = "Bearer",
+               },
+           },
+           new string[] {}
+        }
+     });
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
