ecf8b7389777ea7f5dcbfb27656a67ba83d38ff9
KJoreth <kjoreth97@gmail.com>
2022-08-04 00:22:14 +0200

    Created data seeder

---
+++ Api/Api.Data/Seeder/EntitiesSeeders/ActorEpisodeSeeder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Data.Seeder.EntitiesSeeders
+{
+    public static class ActorEpisodeSeeder
+    {
+        public static void SeedActorEpisode(this ModelBuilder builder)
+        {
+            List<ActorEpisode> relations = new();
+            string[] fileLines = File.ReadAllLines("../Api.Data/Seeder/EntitiesSeeders/Data/ActorEpisode.txt");
+            foreach (string line in fileLines)
+            {
+                var item = GetItemFromLine(line);
+                relations.Add(item);
+            }
+
+            builder.Entity<ActorEpisode>().HasData(relations);
+        }
+        private static ActorEpisode GetItemFromLine(string line)
+        {
+            string[] item = line.Split('|');
+            int actorId = Convert.ToInt32(item[0]);
+            int episodeId = Convert.ToInt32(item[1]);
+
+            return new ActorEpisode() { ActorId = actorId, EpisodeId = episodeId }; 
+        }
+    }
+}
---
+++ Api/Api.Data/Seeder/EntitiesSeeders/ActorsSeeder.cs
@@ -0,0 +1,26 @@
+﻿namespace Api.Data.Seeder.EntitiesSeeders
+{
+    public static class ActorsSeeder
+    {
+        public static void SeedActors(this ModelBuilder builder)
+        {
+            List<Actor> actors = new List<Actor>();
+            string[] fileLines = File.ReadAllLines("../Api.Data/Seeder/EntitiesSeeders/Data/Actors.txt");
+            foreach (string line in fileLines)
+            {
+                var item = GetItemFromLine(line);
+                actors.Add(item);
+            }
+
+            builder.Entity<Actor>().HasData(actors);
+        }
+
+        private static Actor GetItemFromLine(string line)
+        {
+            string[] item = line.Split('|');
+            int id = Convert.ToInt32(item[0]);
+            string name = item[1];
+            return new Actor() { ActorId = id, ActorName = name };
+        }
+    }
+}
